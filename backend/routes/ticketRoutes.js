3d47aa5f517208261a00813e580e5e3d570f403e
Dimitrescu Leonard <dimitresculeonard@gmail.com>
2023-01-26 23:01:47 +0200

    Added new routes for ticket model

--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const { getTickets, createTicket } = require("../controller/ticketController");
+const {
+  getTickets,
+  createTicket,
+  getTicket,
+  updateTicket,
+  deleteTicket,
+} = require("../controller/ticketController");
 
 const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").get(protect, getTickets).post(protect, createTicket);
+router
+  .route("/:id")
+  .get(protect, getTicket)
+  .delete(protect, deleteTicket)
+  .put(protect, updateTicket);
 
 module.exports = router;
